7e908eee69df06544a12329a91702c61417edddd
cassidymanuel01 <cassidymanuel.59@gmail.com>
2022-06-14 11:00:05 +0200

    https://github.com/cassidymanuel01/factoryFunctions.git

--- code.js
+++ code.js
@@ -4,17 +4,13 @@ function practice(name, hours, rate) {
         salary: ()=>{
             return hours * rate;
         },
-        display: () => {
-            document.write(`
-            <br>Name: ${name} <br>
-                Salary: R${hours * rate}<br>`
-            );
-        }
     }
 }
 
 let employee1 = practice('Peter', 40, 500);
 let employee2 = practice('Samuel', 80, 300);
 
-employee1.display();
-employee2.display();
+document.write(`<br>Name: ${employee1.name}<br>
+Salary: R ${employee1.salary()}<br>`) ;
+document.write(`<br>Name: ${employee2.name}<br>
+Salary: R ${employee2.salary()}<br>`) ;
